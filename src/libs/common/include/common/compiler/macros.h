13a8e7d56c4d389d07429e7594a16001a5bfcdd7
ly <1175529741@qq.com>
2026-01-01 15:25:34 +0800

    format file

--- src/libs/common/include/common/compiler/macros.h
+++ src/libs/common/include/common/compiler/macros.h
@@ -4,45 +4,45 @@
 // 平台区分宏
 #if defined(_WIN32) || defined(_WIN64)
 #define PLATFORM_WINDOWS 1
-#define PLATFORM_LINUX 0
-#define PLATFORM_MACOS 0
+#define PLATFORM_LINUX   0
+#define PLATFORM_MACOS   0
 #elif defined(__linux__) || defined(linux)
 #define PLATFORM_WINDOWS 0
-#define PLATFORM_LINUX 1
-#define PLATFORM_MACOS 0
+#define PLATFORM_LINUX   1
+#define PLATFORM_MACOS   0
 #elif defined(__APPLE__)
 #define PLATFORM_WINDOWS 0
-#define PLATFORM_LINUX 0
-#define PLATFORM_MACOS 1
+#define PLATFORM_LINUX   0
+#define PLATFORM_MACOS   1
 #else
 #define PLATFORM_WINDOWS 0
-#define PLATFORM_LINUX 0
-#define PLATFORM_MACOS 0
+#define PLATFORM_LINUX   0
+#define PLATFORM_MACOS   0
 #endif
 
 #if defined(_MSC_VER)
-#define COMPILER_MSVC 1
-#define COMPILER_GCC 0
+#define COMPILER_MSVC  1
+#define COMPILER_GCC   0
 #define COMPILER_CLANG 0
 #define COMPILER_MINGW 0
 #elif defined(__MINGW32__) || defined(__MINGW64__)
-#define COMPILER_MSVC 0
-#define COMPILER_GCC 0
+#define COMPILER_MSVC  0
+#define COMPILER_GCC   0
 #define COMPILER_CLANG 0
 #define COMPILER_MINGW 1
 #elif defined(__clang__)
-#define COMPILER_MSVC 0
-#define COMPILER_GCC 0
+#define COMPILER_MSVC  0
+#define COMPILER_GCC   0
 #define COMPILER_CLANG 1
 #define COMPILER_MINGW 0
 #elif defined(__GNUC__)
-#define COMPILER_MSVC 0
-#define COMPILER_GCC 1
+#define COMPILER_MSVC  0
+#define COMPILER_GCC   1
 #define COMPILER_CLANG 0
 #define COMPILER_MINGW 0
 #else
-#define COMPILER_MSVC 0
-#define COMPILER_GCC 0
+#define COMPILER_MSVC  0
+#define COMPILER_GCC   0
 #define COMPILER_CLANG 0
 #define COMPILER_MINGW 0
 #endif
